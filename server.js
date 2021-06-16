5afd05ab1f8836ad7dd23027bc1f449b9691bcf8
mateuszsm1smiech <75763523+mateuszsm1smiech@users.noreply.github.com>
2021-06-16 08:37:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const socketIO = require("socket.io");
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 var mongoClient = require("mongodb").MongoClient;
 
